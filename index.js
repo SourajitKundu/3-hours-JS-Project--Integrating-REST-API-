a64a967f2df16a08ca3a374dd349f800b4929f89
SourajitKundu <158011701+SourajitKundu@users.noreply.github.com>
2025-06-06 12:25:13 +0530

    Project complete

---
+++ index.js
@@ -0,0 +1,64 @@
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const noteDetails = {
+    noteTitle: event.target.notetitle.value,
+    noteDesc: event.target.notedesc.value,
+  };
+  axios
+    .post(
+      "https://crudcrud.com/api/c65068f9f0af4103a9c445fdc7d5e26a/NoteDetails",
+      noteDetails
+    )
+    .then((response) => displayNoteOnScreen(response.data))
+    .catch((error) => alert("Did not work correctly" + error));
+
+  document.getElementById("notetitle").value = "";
+  document.getElementById("notedesc").value = "";
+}
+
+function displayNoteOnScreen(note) {
+  const noteItem = document.createElement("li");
+
+  const title = document.createElement("h2");
+  title.textContent = note.noteTitle;
+
+  const desc = document.createElement("p");
+  desc.textContent = note.noteDesc;
+
+  noteItem.appendChild(title);
+  noteItem.appendChild(desc);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  noteItem.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click", function (e) {
+    noteList.removeChild(e.target.parentElement);
+
+    axios
+      .delete(
+        `https://crudcrud.com/api/c65068f9f0af4103a9c445fdc7d5e26a/NoteDetails/${note._id}`
+      )
+      .then((res) => alert("Successfully Deleted"))
+      .catch((err) => alert(err));
+  });
+
+  const noteList = document.querySelector("ul");
+  noteList.appendChild(noteItem);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  axios
+    .get(
+      "https://crudcrud.com/api/c65068f9f0af4103a9c445fdc7d5e26a/NoteDetails"
+    )
+    .then((response) => {
+      response.data.forEach((note) => displayNoteOnScreen(note));
+
+      const count = response.data.length;
+      const countNo = document.getElementById("counting");
+      countNo.textContent = `Total Notes : ${count}`;
+    })
+    .catch((err) => console.log("err"));
+});
+
+module.exports = handleFormSubmit;
